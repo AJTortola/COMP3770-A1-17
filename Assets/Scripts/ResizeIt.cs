8a34a61e7a662d5418555394277a6d6dee4ae10c
Abdul <arifb@uwindsor.ca>
2021-09-27 22:07:48 -0400

    ResizeIt updated

--- Assets/Scripts/ResizeIt.cs
+++ Assets/Scripts/ResizeIt.cs
@@ -1,18 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ResizeIt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ public Vector3 ScaleChange = new Vector3(4, 4, 4);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
